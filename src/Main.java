158f96cc9206cd16ef7ff98adfcc37a258b937cc
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-03-04 16:48:45 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static int taxEarningsMinusSpendings(int earnings, int spendings) {
         int tax = (earnings - spendings) * 15 / 100;
-        if (tax >=0) {
-            return tax;
-        }  else {
-            return 0;
-        }
+        return (tax >= 0) ? tax : 0;
     }
 
     public static int taxEarnings(int earnings) {
